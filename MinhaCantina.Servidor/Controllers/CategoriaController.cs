949481dc33c5c619c61948b1d96b6f2ca1a90318
Gaaldra <55445484+Gaaldra@users.noreply.github.com>
2025-12-05 13:22:15 -0300

    Enviando o servidor iniciado

---
+++ MinhaCantina.Servidor/Controllers/CategoriaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinhaCantina.Biblioteca.Modelos;
+
+namespace MinhaCantina.Servidor.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult PegarCategoria()
+	{
+		var categoria = Categoria.Criar("Salgados");
+		return StatusCode(200, categoria);
+	}
+}
---
+++ MinhaCantina.Servidor/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinhaCantina.Biblioteca.Modelos;
+
+namespace MinhaCantina.Servidor.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutoController : ControllerBase
+{
+	
+}
