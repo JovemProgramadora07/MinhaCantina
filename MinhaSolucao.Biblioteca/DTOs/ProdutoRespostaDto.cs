8a34be02a5a490c16799da6a3c0dce1967c3482b
txynxrxc <yatcarneiro05@gmail.com>
2026-02-24 16:56:26 -0300

    Aprimora os cards dos produtos

--- MinhaSolucao.Biblioteca/DTOs/ProdutoRespostaDto.cs
+++ MinhaSolucao.Biblioteca/DTOs/ProdutoRespostaDto.cs
@@ -4,6 +4,7 @@ public class ProdutoRespostaDto
 {
 	public int Id { get; set; }
 	public string Nome { get; set; } = string.Empty;
+	public string Descricao { get; set; } = string.Empty;
 	public decimal Preco { get; set; }
 	public string CategoriaNome { get; set; } = string.Empty;
 }
